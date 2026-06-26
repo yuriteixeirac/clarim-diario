3efc796a743b7dd82874fc14bff0b068149fce96
Yuri Teixeira <yuriteixeirac@proton.me>
2026-06-26 14:54:25 -0300

    feat: finalização do router

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
 
-require "vendor/autoload.php";
+require "../vendor/autoload.php";
 
-echo "Hello, world!";
+use App\Router;
+
+$router = new Router();
+
+$router->get("/", function () {
+    ob_start();
+
+    echo "Hello, world!<br>";
+    echo "Teste de router...";
+
+    return ob_get_contents();
+});
+
+$router->dispatch($_SERVER["REQUEST_URI"], $_SERVER["REQUEST_METHOD"]);
--- src/Router.php
+++ src/Router.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App;
+
 class Router
 {
     private array $routes = [
